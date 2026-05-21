751d887b1f722226cfdc200828880c5bbdcb25cb
yimiao <yimiao9036@outlook.com>
2026-05-21 15:52:30 +0800

    一个导出错误修正

--- predict.py
+++ predict.py
@@ -3,11 +3,11 @@ import ultralytics
 from train import predict_image
 
 if __name__ == "__main__":
-    predict_image('runs/detect/train/train-10/weights/best.pt', 'test.jpg')
+    predict_image('save_models/yolo26n-best.pt', 'test.jpg')
     from ultralytics import YOLO
 
     # 加载你的模型
-    model = YOLO('save_models/best.pt')
+    model = YOLO('save_models/yolo26n-best.pt')
 
     # 导出模型
     model.export(format='onnx',opset=12)
