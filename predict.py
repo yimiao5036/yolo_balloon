87243937aaab944c71a68080af144d7261239ece
yimiao <yimiao9036@outlook.com>
2026-05-21 15:54:25 +0800

    1

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     predict_image('save_models/yolo26n-best.pt', 'test.jpg')
     from ultralytics import YOLO
 
-    # 加载你的模型
+    # 加载模型
     model = YOLO('save_models/yolo26n-best.pt')
 
     # 导出模型
